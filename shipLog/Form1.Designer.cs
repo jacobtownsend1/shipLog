2a5d6a55154f843e6111507cca02f26cbe18f16c
Jacob Townsend <physicsone23@gmail.com>
2020-10-29 22:41:05 -0500

    Just adding first edition of the code!

---
+++ shipLog/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace shipLog
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(280, 74);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "receive";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(340, 74);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(56, 32);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "ship";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 81);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(261, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 66);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ticket Number";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(11, 121);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.ShowCellToolTips = false;
+            this.dataGridView1.Size = new System.Drawing.Size(628, 316);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(118, 34);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label2.Size = new System.Drawing.Size(164, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "(please open or create a new file)";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "xlsx";
+            this.openFileDialog1.FileName = "shiplog";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(650, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.newFileToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
+            this.fileToolStripMenuItem.Text = "file";
+            this.fileToolStripMenuItem.Click += new System.EventHandler(this.fileToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(145, 22);
+            this.openToolStripMenuItem.Text = "open existing";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // newFileToolStripMenuItem
+            // 
+            this.newFileToolStripMenuItem.Name = "newFileToolStripMenuItem";
+            this.newFileToolStripMenuItem.Size = new System.Drawing.Size(145, 22);
+            this.newFileToolStripMenuItem.Text = "new file";
+            this.newFileToolStripMenuItem.Click += new System.EventHandler(this.newFileToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.aboutToolStripMenuItem.Text = "about";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "xlsx";
+            this.saveFileDialog1.FileName = "shiplog";
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "CURRENT FILE:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(401, 74);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(81, 32);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "check status";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(650, 448);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Shipping Log";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ shipLog/Form1.cs
@@ -0,0 +1,328 @@
+﻿using Microsoft.Office.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClosedXML;
+using ClosedXML.Excel;
+using DocumentFormat.OpenXml.Bibliography;
+using System.IO;
+
+namespace shipLog
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        //setting up some variables we'll need later in multiple properties and/or functions
+        DataTable myDt = new DataTable();
+        XLWorkbook currentWb;
+        IXLWorksheet currentWsh;
+        string filename;
+
+        /*
+         * 
+         * 
+         * FUNCTIONAL METHODS
+         * 
+         * 
+         */
+
+
+
+        //method to return the row in an excel file that a certain item occurs at in the index
+        public int atIndex(IXLWorksheet sheet, string con)
+        {
+            
+             int item; 
+             for(item = 1; item <= sheet.LastRowUsed().RowNumber(); item++)
+             {
+                if(sheet.Cell(item, 1).Value.ToString() == con)
+                {
+                    return item;
+                }
+             }
+            
+            return 0;
+        }
+
+        //method to import an excel file to a data table. 
+        //I will use this later in ways that are not particularly efficient, but I couldn't think of any better ideas. Please don't judge me, I'm working on fixing it
+        //UPDATE: I fixed it, but I'm leaving this message here just to brighten your data little :)
+        public static DataTable ImportExcelToDT(IXLWorksheet workSheet)
+        {
+            DataTable dt = new DataTable();
+
+            bool firstRow = true;
+            foreach (IXLRow row in workSheet.Rows())
+            {
+                if (firstRow)
+                {
+                    foreach (IXLCell cell in row.Cells())
+                    {
+                        dt.Columns.Add(cell.Value.ToString());
+                    }
+                    firstRow = false;
+                }
+                else
+                {
+                    dt.Rows.Add();
+                    int i = 0;
+                    foreach (IXLCell cell in row.Cells(row.FirstCellUsed().Address.ColumnNumber, row.LastCellUsed().Address.ColumnNumber))
+                    {
+                        dt.Rows[dt.Rows.Count - 1][i] = cell.Value.ToString();
+                        i++;
+                    }
+                }
+            }
+            return dt;
+        }
+
+        //refreshes the data grid view
+        public static void xlDataGridRefresh(DataGridView dgv, XLWorkbook work, DataTable dt)
+        {
+            dgv.DataSource = null;
+            dgv.Rows.Clear();
+            dgv.DataSource = dt;
+        }
+
+        /*
+         * 
+         * 
+         * functional objects(i.e. buttons, etc that do stuff)
+         * 
+         * 
+         */
+
+        //receiveing button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            //make sure the workbook file is selected
+            if (currentWb == null)
+            {
+                MessageBox.Show("You must select a file!");
+                return;
+            }
+
+            //make sure the ticket isn't left blank. This check isn't perfect, but a better one is in the works ;)
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("You can't receive nothing!");
+                return;
+            }
+
+            //check if the ticket has already been received or not
+            if (atIndex(currentWsh, textBox1.Text) > 0)
+            {
+                MessageBox.Show("This ticket has already been received!");
+            }
+            else
+            {
+                //create a new row, add the ticket, and update the received date for said ticket.
+                int rowNumber = currentWsh.LastRowUsed().RowNumber() + 1;
+                currentWsh.Cell(rowNumber, 1).Value = textBox1.Text;
+                currentWsh.Cell(rowNumber, 2).Value = DateTime.Now;
+                myDt.Rows.Add(textBox1.Text, DateTime.Now);
+                currentWb.Save();
+
+                xlDataGridRefresh(dataGridView1, currentWb, myDt);
+
+                MessageBox.Show("Ticket received successfully!");
+            }
+
+            
+        }
+
+        //shipping button
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (currentWb == null)
+            {
+                MessageBox.Show("You must select a file!");
+                return;
+            }
+
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("You can't receive nothing!");
+                return;
+            }
+
+            //check if the ticket has been marked as received
+            int index = atIndex(currentWsh, textBox1.Text);
+            if(index > 0)
+            {
+                //check if the ticket has been marked as shipped already
+                if(currentWsh.Cell(index, 3).Value.ToString() != "")
+                {
+                    MessageBox.Show("This ticket has already been shipped!");
+                }
+                else {
+                    currentWsh.Cell(index, 3).Value = DateTime.Now;
+                    myDt.Rows[index - 2]["shipped"] = DateTime.Now;
+                    currentWb.Save();
+
+                    xlDataGridRefresh(dataGridView1, currentWb, myDt);
+
+                    MessageBox.Show("Ticket has been market as shipped!");
+                }
+            } else
+            {
+                MessageBox.Show("Can't mark ticket as shipped. Has the ticket been received?");
+            }
+        }
+
+        //check ticket status button
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //make sure file is selected..
+            if (currentWb == null)
+            {
+                MessageBox.Show("You must select a file!");
+                return;
+            }
+
+            //make sure textbox isn't empty...
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("You can't receive nothing!");
+                return;
+            }
+
+            //make sure the ticket exists in the index column...
+            if (atIndex(currentWsh, textBox1.Text) != 0)
+            {
+                //set up some variables that will be useful for readability sake
+                var recieved = currentWsh.Cell(atIndex(currentWsh, textBox1.Text), 2).Value;
+                var shipped = currentWsh.Cell(atIndex(currentWsh, textBox1.Text), 3).Value;
+
+
+                //check if the ticket has been marked as received
+                if ((recieved.ToString() != "") && (recieved != null))
+                {
+                    //given it's received, check if it was shipped...
+                    if ((shipped.ToString() != "") && (shipped != null))
+                    {
+                        MessageBox.Show(string.Format("Ticket #{0} was received at {1} and shipped at {2}", textBox1.Text, recieved.ToString(), shipped.ToString()));
+                    }
+                    else
+                    {
+                        MessageBox.Show(string.Format("Ticket #0 was received at {1} but was not shipped!", textBox1.Text, recieved.ToString()));
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Ticket does not exist!");
+            }
+        }
+
+        
+
+
+        // TOOLSTRIP ITEMS
+
+        //open an existing file
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //initialize our important variables
+                filename = openFileDialog1.FileName;
+                currentWb = new XLWorkbook(filename);
+                currentWsh = currentWb.Worksheet(1);
+                myDt = ImportExcelToDT(currentWsh);
+
+                //UI stuff to make the program display right
+                label2.Text = filename;
+                dataGridView1.DataSource = myDt;
+            }
+        }
+
+        //create a new file
+        private void newFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK) { 
+                //initialize our needed variables
+                currentWb = new XLWorkbook();
+                filename = saveFileDialog1.FileName;
+                currentWsh = currentWb.AddWorksheet("Sheet1");
+
+                //set up the worksheet to our specifications
+                currentWsh.Cell(1, 1).Value = "ticket";
+                currentWsh.Cell(1, 2).Value = "received";
+                currentWsh.Cell(1, 3).Value = "shipped";
+
+                //save everything and finish initializing our variables
+                currentWb.SaveAs(filename);
+                myDt = ImportExcelToDT(currentWsh);
+
+                //update UI stuff so the program displays right
+                dataGridView1.DataSource = myDt;
+                label2.Text = filename;
+            }
+        }
+
+        //this one is self-explanatory
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Created by Jacob Townsend \nfirst built on: 10-17-2020");
+        }
+        
+        /*
+         * 
+         * 
+         * stuff I didn't really use but had to keep here because if I didn't the program wouldn't run lol
+         * 
+         * 
+         */
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
